8f31b74377834377da107b26d530c2ce64bddc1d
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-05-04 09:28:37 -0400

    Additional updates
     to add thumbnail images also style changes.

---
+++ db/migrate/20170503201138_add_thumbnail_image_to_sections.rb
@@ -0,0 +1,14 @@
+class AddThumbnailImageToSection < ActiveRecord::Migration[5.0]
+  def up
+    add_column 'sections', 'filename', :string
+    add_column 'sections', 'content_type', :string
+    # long blob
+    add_column 'sections', 'file_contents', :binary, limit: 16_777_215
+  end
+
+  def down
+    remove_column 'sections', 'file_contents'
+    remove_column 'sections', 'content_type'
+    remove_column 'sections', 'filename'
+  end
+end
